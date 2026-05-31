d65aa092ba581e30e3d3c0f613f8cc92173ff096
riteshdhakulkar <riteshdhakulkar1905@gmail.com>
2026-05-31 18:15:09 +0530

    fix api url for production

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,15 @@
-const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000"
-    : "https://your-backend-url.onrender.com";
+// // const API_URL =
+// //   process.env.NODE_ENV === "development"
+// //     ? "http://localhost:4000"
+// //     : "https://your-backend-url.onrender.com";
+
+// // export default API_URL;
+// const API_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:4000"
+//     : "https://nexcartify-ecommerce.onrender.com";
+
+// export default API_URL;
+const API_URL = "https://nexcartify-ecommerce.onrender.com";
 
 export default API_URL;
