71a4b5975e520c9c877638c634937af63e2d2f5d
riteshdhakulkar <riteshdhakulkar1905@gmail.com>
2026-06-02 22:51:30 +0530

    Fix deployment issues

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollections.css'
-
-
-import Item from '../Items/Item'
+import React, { useEffect, useState } from 'react';
+import './NewCollections.css';
+import Item from '../Items/Item';
+import API_URL from '../../config/api';
 
 const NewCollections = () => {
-  const [new_collection,setNew_Collection] = useState([]);
-  useEffect(()=>{
- fetch('http://localhost:4000/newcollections')
- .then((response)=>response.json())
- .then((data)=>setNew_Collection(data));
-  },[])
-  return (
 
+  const [new_collection, setNew_Collection] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/newcollections`)
+      .then((response) => response.json())
+      .then((data) => setNew_Collection(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
     <div className='new-collections' id='new-collections'>
 
       <h1>NEW COLLECTIONS</h1>
@@ -21,26 +23,21 @@ const NewCollections = () => {
 
       <div className="collections">
 
-        {new_collection.map((item, i) => {
-
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          )
-
-        })}
+        {new_collection.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
 
       </div>
 
     </div>
+  );
+};
 
-  )
-}
-
-export default NewCollections
+export default NewCollections;
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
+import React, { useEffect, useState } from 'react';
+import './Popular.css';
+import Item from '../Items/Item';
+import API_URL from '../../config/api';
 
-import Item from '../Items/Item'
 const Popular = () => {
 
-  const [popularProduct,setPopularProduct] =  useState([]);
-  useEffect(()=>{
- fetch('http://localhost:4000/popularinwomen')
- .then((response)=>response.json())
- .then((data)=>setPopularProduct(data));
-  },[])
+  const [popularProduct, setPopularProduct] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/popularinwomen`)
+      .then((response) => response.json())
+      .then((data) => setPopularProduct(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr />
+
       <div className="popular-item">
-        {popularProduct.map((item , i)=>{
-            return <Item key={i} id={item.id} name={item.name}
-             image={item.image} new_price={item.new_price}
-             old_price={item.old_price}
-             />
-        })}
+        {popularProduct.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;
